274408cb5791573f1e175585609afbf3f02bfdd9
monseeligio <71795808+monseeligio@users.noreply.github.com>
2020-11-26 14:41:43 -0600

    Create Nodo.java

---
+++ Nodo.java
@@ -0,0 +1,12 @@
+
+package u3e4pilanumeros;
+public class Nodo {
+   int elemento;
+    Nodo Siguiente;
+ 
+    public  Nodo (int elemento){
+        this.elemento=elemento;
+        Siguiente=null; 
+        
+}
+}
